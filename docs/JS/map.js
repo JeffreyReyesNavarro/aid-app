5de598f5313eae23f332b9d14ac65a324dd99af8
Jeffrey Navarro <navarrojef@gmail.com>
2018-10-06 04:36:24 +0930

    fix the design and layout of the web app

--- docs/JS/map.js
+++ docs/JS/map.js
@@ -1,9 +1,11 @@
 var map, infoWindow;
+
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -12.462827, lng: 130.841782},
           zoom: 13
         });
+
         infoWindow = new google.maps.InfoWindow;
 
         // Try HTML5 geolocation.
@@ -13,12 +15,19 @@ var map, infoWindow;
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            
+
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: (pos),
+                radius: 2000,
+                type: ['supermarket']
+            }, callback);
+
             var marker = new google.maps.Marker({
                 position:(pos),
                 map:map
             });
-            
+
             map.setCenter(pos);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
@@ -30,6 +39,28 @@ var map, infoWindow;
 
        
       }
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
+
+      function createMarker(place) {
+
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
+
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
